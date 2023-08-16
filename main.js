1f0490e7eaa5b0da0cd29235e6a2f1dcd5e1436c
Adam Hembrough <adam@magic-music.co.uk>
2023-08-16 13:08:51 +0100

    Create event tunnel from main to audio window

--- main.js
+++ main.js
@@ -11,13 +11,41 @@ const createWindow = () => {
             enableRemoteModule: false, // turn off remote
             preload: path.join(__dirname, 'boot.js')
         },
+
+    })
+
+    const audioWin = new BrowserWindow({
+        minimizable: true,
+        webPreferences: {
+            nodeIntegration: true, // is default value after Electron v5
+            contextIsolation: true, // protect against prototype pollution
+            enableRemoteModule: false, // turn off remote
+            preload: path.join(__dirname, 'bootAudio.js')
+        },
+    })
+
+    ipcMain.on('playAudio', () => {
+        audioWin.webContents.send('playAudio')
+    })
+
+    ipcMain.on('stopAudio', () => {
+        audioWin.webContents.send('stopAudio')
     })
 
     win.loadFile('home/home.html')
     win.setMenu(null)
-    
+
+    audioWin.loadFile('home/audio.html')
+    audioWin.setMenu(null)
+    audioWin.minimize()
+
+    win.on('closed', () => {
+        audioWin.close()
+    })
+
     //Uncomment to show developer tools
     //win.webContents.openDevTools()
+    //audioWin.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
